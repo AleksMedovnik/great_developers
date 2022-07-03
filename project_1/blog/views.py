b48bf8b7f00d51f7f07bb1a40dc756e1c2c99aed
Aleksandr <medovnikaleksander@yandex.ua>
2022-07-03 21:54:21 +0300

    Use APIView

--- project_1/blog/views.py
+++ project_1/blog/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Person
 from .serializers import PersonSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
-class PersonAPIViews(generics.ListAPIView):
-    queryset = Person.objects.all()
-    serializer_class = PersonSerializer
+# class PersonAPIViews(generics.ListAPIView):
+#     queryset = Person.objects.all()
+#     serializer_class = PersonSerializer
+
+
+class PersonAPIViews(APIView):
+   def get(self, request):
+       return Response({'title', 'Linus'})
